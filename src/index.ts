c72fb4b7f392ccd9dcc5bcb49f96ccc400769508
F3xeek <filipziarko7@gmail.com>
2025-06-06 09:37:47 +0200

    prevent caching of css files

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import { gets } from "./api"
 
 window.addEventListener("load", async () => {
+    const styles = document.getElementById("styles") as HTMLLinkElement
+    if (styles) styles.href = styles.href + "?v='" + new Date().getTime();
+
     const data = await gets.getPlayerGame()
-    if(data.status=="success") location.href = "lobby.html";
+    if (data.status == "success") location.href = "lobby.html";
     const submitbutton = document.getElementById("joinButton")
     const nicknameInput = document.getElementById("nicknameInput") as HTMLInputElement;
     submitbutton?.addEventListener("click", async () => {
-        if (nicknameInput.value.length>0){
+        if (nicknameInput.value.length > 0) {
             let result = await gets.joinGame(nicknameInput.value);
-            if (result.status== "success") location.href = "lobby.html"
+            if (result.status == "success") location.href = "lobby.html"
             else alert(result.message)
-        }else alert("Nick nie może być pusty!")
+        } else alert("Nick nie może być pusty!")
     })
 
 })
--- src/lobby.ts
+++ src/lobby.ts
@@ -1,17 +1,21 @@
-import {gets} from "./api"
+import { gets } from "./api"
 
-window.addEventListener("load",async () => {
+window.addEventListener("load", async () => {
+    const styles = document.getElementById("styles") as HTMLLinkElement
+    if (styles) styles.href = styles.href + "?v='" + new Date().getTime();
+
     const data = await gets.getPlayerGame();
-    if (data.status == "fail") {alert(data.message); location.href = "/zolwie"}
-    else if(data.players){
-        data.players.forEach((player,id) => {
-            const cell = document.getElementById("player"+(id+1))
+    if (data.status == "fail") { alert(data.message); location.href = "/zolwie" }
+    else if (data.players) {
+        data.players.forEach((player, id) => {
+            const cell = document.getElementById("player" + (id + 1))
             if (cell) {
                 cell.innerText = player.name
-                console.log(data.selfId == player.id, data.selfId , player.id);
-                if (data.selfId==player.id) cell.classList.add("self")
+                console.log(data.selfId == player.id, data.selfId, player.id);
+                if (data.selfId == player.id) cell.classList.add("self")
             }
         });
         console.log(data.players);
     }
+
 });
